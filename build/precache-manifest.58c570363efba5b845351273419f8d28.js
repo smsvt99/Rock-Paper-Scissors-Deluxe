f987a6561570b8fb83ca5fd27795ec82ce01fd08
Sean Stone <smstone@uvm.edu>
2019-04-26 17:00:38 -0400

    inter alia, unignored build

---
+++ build/precache-manifest.58c570363efba5b845351273419f8d28.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d24de4a0f7905de868f356306c16df",
+    "url": "/static/media/testsprites.a3d24de4.png"
+  },
+  {
+    "revision": "71356969a3a145f42a7fdd478373ed73",
+    "url": "/static/media/hands.71356969.png"
+  },
+  {
+    "revision": "bc6c92384fa84b1c3f85f25474ae04d5",
+    "url": "/static/media/earthclock.bc6c9238.jpg"
+  },
+  {
+    "revision": "81c0aae9bb2eb54bf8fe07e1b68d829e",
+    "url": "/static/media/PressStart2P-Regular.81c0aae9.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9c19fb1a763c9999b0a",
+    "url": "/static/js/main.154c1f39.chunk.js"
+  },
+  {
+    "revision": "9abbd59ce7436f12d5da",
+    "url": "/static/js/2.22fddd3d.chunk.js"
+  },
+  {
+    "revision": "f9c19fb1a763c9999b0a",
+    "url": "/static/css/main.8eec96c5.chunk.css"
+  },
+  {
+    "revision": "425bde3662e3fdfb6609a5c61d108fa4",
+    "url": "/index.html"
+  }
+];
