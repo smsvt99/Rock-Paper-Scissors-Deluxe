b7ca914f672cc6e9591e0e2bd0f5cb254fdef283
Sean Stone <smstone@uvm.edu>
2019-04-26 14:36:34 -0400

    wip

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join('public')))
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
