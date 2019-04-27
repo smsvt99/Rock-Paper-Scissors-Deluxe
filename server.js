fa9b3bbe431e33571f7a82a0f75173b0f8739b55
Sean Stone <smstone@uvm.edu>
2019-04-27 10:02:39 -0400

    changed socket.on to io.on

--- server.js
+++ server.js
@@ -30,14 +30,14 @@ io.on('connection', socket => {
     console.log('new connection: ' + socket.id);
     
     io.to(socket.id).emit('socket_id', socket.id)
-    
-    socket.on('initial_info', info =>{
+    //socket
+    io.on('initial_info', info =>{
         console.log(info)
         let target = info.enemy_id;
         io.to(target).emit('initial_info_from_server', info)
     })
-    
-    socket.on('moves_from_client', info =>{
+    //socket
+    io.on('moves_from_client', info =>{
         console.log(info)
         let target = info.enemy_id;
         io.to(target).emit('moves_from_server', info)
